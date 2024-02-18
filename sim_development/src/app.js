e2d62e3038adc2c9079ce6b31239f9c3d2f64f31
Witold Kepczynski <witold.kepczynski@gmail.com>
2024-02-18 23:36:20 +0100

    Delete app.js, add .gitignore and package.json

--- sim_development/src/app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var simulator_worker_1 = require("../../src/app/sevices/simulator/simulator-worker");
-console.log('Starting simulator');
-var sim = new simulator_worker_1.Simulator();
